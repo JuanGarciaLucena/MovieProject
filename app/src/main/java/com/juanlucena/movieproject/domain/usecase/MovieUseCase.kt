f0dec1277fa28777ba9407d6b582f34f78b916de
JuanLucena <juangarcialucena@gmail.com>
2022-11-19 16:37:57 +0100

    F005 -> get all movies use case

---
+++ app/src/main/java/com/juanlucena/movieproject/domain/usecase/MovieUseCase.kt
@@ -0,0 +1,23 @@
+package com.juanlucena.movieproject.domain.usecase
+
+import com.juanlucena.movieproject.data.database.entity.toEntity
+import com.juanlucena.movieproject.data.repository.MovieRepository
+import com.juanlucena.movieproject.domain.model.Movie
+import com.juanlucena.movieproject.domain.model.toDomain
+import javax.inject.Inject
+
+class MovieUseCase @Inject constructor(private val repository: MovieRepository) {
+
+    suspend fun getAllMovies() : List<Movie>{
+
+        val movieListFromDb = repository.getAllMoviesFromDatabase()
+
+        return if(movieListFromDb.isEmpty()){
+            val movieList = repository.getAllMoviesFromApi().map { movieModel -> movieModel.toEntity() }
+            repository.insertAllMovies(repository.getAllMoviesFromApi().map { movieModel -> movieModel.toEntity() })
+            movieList.map { movieEntity -> movieEntity.toDomain() }
+        }else{
+            movieListFromDb.map { movieEntity -> movieEntity.toDomain() }
+        }
+    }
+}
